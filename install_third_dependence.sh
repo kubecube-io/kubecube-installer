13b4ad02ae37d34f12d11d8bd02f12cffdf25a9f
weilaaa <weilanidaye@gmail.com>
2021-05-27 12:47:56 +0800

    add shells

--- install_third_dependence.sh
+++ install_third_dependence.sh
@@ -11,7 +11,16 @@ echo -e "\033[32m================================================\033[0m"
 echo -e "\033[32m>>>>>>	deploy hnc-manager...\033[0m"
 kubectl apply -f manifests/hnc/hnc.yaml
 
-sleep 60 >/dev/null
+hnc_ready="0/2"
+while [ ${hnc_ready} != "2/2" ]
+do
+  sleep 5 > /dev/null
+  hnc_ready=$(kubectl get pod -n hnc-system | awk '{print $2}' | sed -n '2p')
+  echo -e "\033[32m>>>>>>	hnc-manager ready status: ${hnc_ready}...\033[0m"
+done
+
+echo -e "\033[32m================================================\033[0m"
+echo -e "\033[32m>>>>>>	hnc-manager ready...\033[0m"
 
 echo -e "\033[32m================================================\033[0m"
 echo -e "\033[32m>>>>>>	deploy metrics-server...\033[0m"
