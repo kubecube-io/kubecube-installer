00f142932a60250fd995ee3d5b225d5001dc606e
weilaaa <weilanidaye@gmail.com>
2021-06-08 16:49:59 +0800

    add shells

---
+++ entry.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+if [ ${UID} -ne 0 ];then
+  echo -e "\033[32m please use root to execute install shell...\033[0m"
+  exit 1
+fi
+
+mkdir kubecube
+cd kubecube
+
+echo -e "\033[32m================================================\033[0m"
+echo -e "\033[32m Download manifests for kubecube...\033[0m"
+echo -e "\033[32m================================================\033[0m"
+wget https://gitee.com/kubecube/manifests/repository/archive/master.zip
+
+has_apt=$(which apt)
+if [ ! -z ${has_apt} ]; then
+  echo -e "\033[32m Update apt...\033[0m"
+  echo -e "\033[32m================================================\033[0m"
+  apt-get update -y
+  apt-get install -y unzip > /dev/null
+fi
+
+has_yum=$(which yum)
+if [ $? -eq 0 ]; then
+  echo -e "\033[32m Update yum...\033[0m"
+  echo -e "\033[32m================================================\033[0m"
+  yum update -y
+  yum install -y unzip > /dev/null
+fi
+
+unzip master.zip > /dev/null
+
+if [ ${CUSTOMIZE} = "true" ]; then
+  echo -e "\033[32m================================================\033[0m"
+  echo -e "\033[32m Please make sure under kubecube folder     \033[0m"
+  echo -e "\033[32m 'cd ./kubecube'                            \033[0m"
+  echo -e "\033[32m Please modify ./manifests/install.conf     \033[0m"
+  echo -e "\033[32m 'vi ./manifests/install.conf'              \033[0m"
+  echo -e "\033[32m Please modify ./manifests/cube.conf        \033[0m"
+  echo -e "\033[32m 'vi ./manifests/cube.conf'                 \033[0m"
+  echo -e "\033[32m Confirm every args then do command below:  \033[0m"
+  echo -e "\033[32m '/bin/bash ./manifests/install.sh'            \033[0m"
+  echo -e "\033[32m================================================\033[0m"
+  exit 0
+fi
+
+/bin/bash ./manifests/install.sh
--- install_kubecube.sh
+++ install_kubecube.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source ./manifests/cube.conf
+
 IPADDR=$(hostname -I |awk '{print $1}')
 
 function sign_cert() {
@@ -62,7 +64,13 @@ echo -e "\033[32m================================================\033[0m"
 echo -e "\033[32m Render Values of KubeCube...\033[0m"
 cat >values.yaml <<EOF
 kubecube:
+  replicas: ${kubecube.replicas}
+  args:
+    logLevel: ${kubecube.args.logLevel}
+    securePort: ${kubecube.args.securePort}
+    webhookServerPort: ${kubecube.args.webhookServerPort}
   env:
+    jwtSecret: ${kubecube.env.jwtSecret}
     pivotCubeHost: ${IPADDR}:30443
 
 webhook:
