6d89ac3aae214fef2c427a67eb6d132593e2c5ed
weilaaa <weilanidaye@gmail.com>
2021-07-15 14:34:14 +0800

    [add] add readyz for hnc

--- entry.sh
+++ entry.sh
@@ -56,7 +56,7 @@ else
   echo -e "$(date +'%Y-%m-%d %H:%M:%S') \033[32mINFO\033[0m downloading manifests for kubecube"
   wget https://gitee.com/kubecube/manifests/repository/archive/master.zip -O manifests.zip
 
-  unzip manifests.zip > /dev/null
+  unzip manifests.zip -d manifests > /dev/null
 fi
 
 if [[ ${CUSTOMIZE} = "true" ]]; then
