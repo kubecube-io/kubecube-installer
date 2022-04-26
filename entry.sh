f26b43b6b6e8a865fee8dec29c8a2e7881443def
weilaaa <weilanidaye@gmail.com>
2022-04-26 11:00:57 +0800

    modify deploy pkg to v1.2

--- entry.sh
+++ entry.sh
@@ -64,7 +64,7 @@ if [ -e "./manifests" ]; then
   echo -e "$(date +'%Y-%m-%d %H:%M:%S') \033[32mINFO\033[0m manifests already exist"
 else
   echo -e "$(date +'%Y-%m-%d %H:%M:%S') \033[32mINFO\033[0m downloading manifests for kubecube"
-  wget https://kubecube.nos-eastchina1.126.net/kubecube-installer/v1.1/manifests.tar.gz -O manifests.tar.gz
+  wget https://kubecube.nos-eastchina1.126.net/kubecube-installer/v1.2/manifests.tar.gz -O manifests.tar.gz
 
   tar -xzvf manifests.tar.gz > /dev/null
 fi
