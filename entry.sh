badc43149f2b2b1a03bdba50280ce0165bc381b1
tiancan <tangxinxing1994@gmail.com>
2023-01-18 17:00:44 +0800

    Update k8s to 1.26 (#50)
    * add k8s 1.26.0 support
    
    * add calico 1.26.0 support
    
    * update taint
    
    * fix modify hostname letter
    
    * add k8s 1.24.7 1.25.3 support
    
    * add k8s 1.24.7 1.25.3 support
    
    * fix 1.24.7 image bug
    
    * add clean shell
    
    * update kubecube to 1.7
    
    * update kubecube to 1.7
    
    * update kubecube images to 1.7
    
    * update user result
    
    * update webconsole env
    
    * update kubecube_version value
    
    * update install func
    
    * update install func
    
    * update entry.sh
    
    Co-authored-by: weilaaa <weilanidaye@gmail.com>

--- entry.sh
+++ entry.sh
@@ -58,20 +58,18 @@ function env_check() {
 
 env_check
 
+mkdir -p /etc/kubecube
 mkdir -p /etc/kubecube/down
 mkdir -p /etc/kubecube/bin
 cd /etc/kubecube
 
 if [ -e "./manifests" ]; then
-  echo -e "$(date +'%Y-%m-%d %H:%M:%S') \033[32mINFO\033[0m manifests and kubecube-chart already exist"
+  echo -e "$(date +'%Y-%m-%d %H:%M:%S') \033[32mINFO\033[0m manifests already exist"
 else
   echo -e "$(date +'%Y-%m-%d %H:%M:%S') \033[32mINFO\033[0m downloading manifests for kubecube"
-  wget https://kubecube.nos-eastchina1.126.net/kubecube-installer/release/v1.3/manifests.tar.gz -O manifests.tar.gz
-  tar -xzvf manifests.tar.gz > /dev/null
+  wget https://kubecube.nos-eastchina1.126.net/kubecube-installer/v1.8/manifests.tar.gz -O manifests.tar.gz
 
-  echo -e "$(date +'%Y-%m-%d %H:%M:%S') \033[32mINFO\033[0m downloading kubecube-chart ${KUBECUBE_VERSION}"
-  wget https://kubecube.nos-eastchina1.126.net/kubecube-chart/${KUBECUBE_VERSION}/kubecube-chart.tar.gz -O kubecube-chart.tar.gz
-  tar -xzvf kubecube-chart.tar.gz > /dev/null
+  tar -xzvf manifests.tar.gz > /dev/null
 fi
 
 if [[ ${CUSTOMIZE} = "true" ]]; then
